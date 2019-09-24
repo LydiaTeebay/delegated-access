240057c0f558711967f1208e9d3b10bfed88490a
monsterlimo <mattnixon79@googlemal.com>
2019-09-24 14:29:01 +0100

    added service select on v2
    also fixed the padding on details component

--- app/routes.js
+++ app/routes.js
@@ -4,20 +4,20 @@ const router = express.Router();
 const http = require('http');
 
 function getVersion(a) {
-    return a || 'v1';
+    return a.url.substring(1,3) || 'v1';
 }
 
 // add scenarios here - change session data
 router.get('/*/alice-nominates-bob', function (req, res) {
     req.session.data['hasLogin'] = true;
     console.log(getVersion(req.query.version))
-    res.render(req.url.substring(1,3) + '/email/delegate-invite');
+    res.render(getVersion(req) + '/email/delegate-invite');
 })
 
 router.get('/*/research-flow-bob', function(req, res) {
     req.session.data = {}; // reset the data
     //console.log('Im here')
-    res.render(getVersion(req.query.version) + '/email/delegate-invite');
+    res.render(getVersion(req) + '/email/delegate-invite');
 })
 
 // testing version controlling routing
@@ -27,7 +27,7 @@ router.get('/*/research-flow-alice', function(req, res) {
     http.get("http://delegated-access-nhslogin.herokuapp.com", function(e) {});
     http.get("http://delegated-access.herokuapp.com/", function(e) {});
     http.get("http://manage-nhs-login.herokuapp.com/", function(e) {});
-    res.redirect('/' + getVersion(req.query.version) + '/primary-user-details');
+    res.redirect('/' + getVersion(req) + '/primary-user-details');
 
 })
 
@@ -36,23 +36,27 @@ router.get('/*/research-flow-alice', function(req, res) {
     http.get("http://delegated-access-nhslogin.herokuapp.com", function(e) {});
     http.get("http://delegated-access.herokuapp.com/", function(e) {});
     http.get("http://manage-nhs-login.herokuapp.com/", function(e) {});
-    res.redirect('/' + getVersion(req.query.version) + '/primary-user-details');
+    res.redirect('/' + getVersion(req) + '/primary-user-details');
 
 })
 
 router.get('/*/alice-adds-bob-as-delegator', function (req, res) {
     req.session.data = {}
     req.session.data['hasLogin'] = true;
-    res.render(getVersion(req.query.version) + 'service/select-single-service');
+    res.render(getVersion(req) + 'service/select-single-service');
 })
 
 router.post('/*/add-delegate/delegate-nhs-no-1', function(req, res) {
-    req.session.data['hasNHSNumber'];
+    //req.session.data['hasNHSNumber'];
+    console.log('quert version ' + req.query.version);
+    console.log('substring ' + req.url.substring(1,3));
+    console.log('hasNHSnumber ' + req.session.data.hasNHSNumber);
+    //console.log('version ' + getVersion(req.query.version));
 
     if (req.session.data['hasNHSNumber'] == 'yes') {
-        res.redirect('/' + getVersion(req.query.version) + '/add-delegate/delegate-nhs-no-2');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-nhs-no-2');
     } else {
-        res.redirect('/' + getVersion(req.query.version) + '/add-delegate/delegate-dob');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-dob');
     }
 })
 
@@ -61,10 +65,10 @@ router.post('/*/nhs-login', function (req, res) {
     var nhsNumber = req.session.data['nhs-login'];
     
     if (nhsNumber == "Yes"){
-      res.redirect('/' + getVersion(req.query.version) + '/login/login-details')
+      res.redirect('/' + getVersion(req) + '/login/login-details')
     }
     else {
-      res.redirect('/' + getVersion(req.query.version) + '/login/login-details')
+      res.redirect('/' + getVersion(req) + '/login/login-details')
     }
 })
 
@@ -80,11 +84,11 @@ router.post('/*/login/login-details', function(req, res) {
             if (serviceConsent === true) {
                 // login then go straight into the service
                 console.log('user has given consent')
-                res.redirect('/' + getVersion(req.query.version) + '/service/service-logged-in')
+                res.redirect('/' + getVersion(req) + '/service/service-logged-in')
             } else {
                 // go to the service consent page
                 console.log('user has not given consent');
-                res.redirect('/' + getVersion(req.query.version) + '/service/nhslogin-consent');
+                res.redirect('/' + getVersion(req) + '/service/nhslogin-consent');
             }
         } else {
         }
@@ -93,22 +97,22 @@ router.post('/*/login/login-details', function(req, res) {
 router.post('/*/add-delegate/delegate-relationship', function(req, res) {
     var relationship = req.session.data['relationship'];
     if (relationship === '1') {
-        res.redirect('/' + req.url.substring(1,3) + '/add-delegate/delegate-relationship-1');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-relationship-1');
 
     } if (relationship === '2') {
-        res.redirect('/' + req.url.substring(1,3) + '/add-delegate/delegate-relationship-2');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-relationship-2');
 
     } if (relationship === '3') {
-        res.redirect('/' + req.url.substring(1,3) + '/add-delegate/delegate-relationship-3');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-relationship-3');
             
     } if (relationship === '4') {
-        res.redirect('/' + req.url.substring(1,3) + '/add-delegate/delegate-relationship-4');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-relationship-4');
    
     } if (relationship === '5') {
-        res.redirect('/' + req.url.substring(1,3) + '/add-delegate/delegate-relationship-5');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-relationship-5');
 
     } else {
-        res.redirect('/' + req.url.substring(1,3) + '/add-delegate/delegate-nhs-no-1');
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-nhs-no-1');
     }
 })
 
@@ -117,30 +121,30 @@ router.get('/*/service/nhslogin-consent', function(req, res) {
     req.session.data['id'] = req.query.id || 'passed';
     console.log("ID check has: " + req.session.data['id']);
 
-    res.render(getVersion(req.query.version) + '/service/nhslogin-consent');
+    res.render(getVersion(req) + '/service/nhslogin-consent');
 })
 
 router.post('/*/service/nhslogin-consent', function(req, res) {
     var givesConsent = req.session.data['givesConsent'];
 
     if (givesConsent === 'yes') {
-        res.redirect('/' + getVersion(req.query.version) + '/service/nhslogin-success');
+        res.redirect('/' + getVersion(req) + '/service/nhslogin-success');
     } else {
-        res.redirect('/' + getVersion(req.query.version) + '/service/nhslogin-rejected');
+        res.redirect('/' + getVersion(req) + '/service/nhslogin-rejected');
     }
 })
 
-router.post('/*/service/select-single-service', function(req, res) {
-    var service = req.session.data['selectSingleService'];
-
-    if (service === 'notFound') {
-        res.redirect('/' + getVersion(req.query.version) + '/service/select-service-not-listed');
-    } else {
-        // set the service throughout
-        req.session.data['service'] = service;
-        res.redirect('/' + getVersion(req.query.version) + '/add-delegate/delegate-preflight');
-    }
-})
+////
+//router.post('/*/service/select-single-service', function(req, res) {
+//    var service = req.session.data['selectSingleService'];
+//
+//    if (service === 'notFound') {
+//        res.redirect('/' + getVersion(req.query.version) + '/service/select-service-not-listed');
+//    } else {
+//        req.session.data['service'] = service;
+//        res.redirect('/' + getVersion(req.query.version) + '/add-delegate/delegate-preflight');
+//    }
+//})
 
 router.post('/*/login-as', function (req, res) {
 
@@ -150,11 +154,11 @@ router.post('/*/login-as', function (req, res) {
     // Check whether the variable matches a condition
     if (proxy == "Yourself"){
       // Send user to next page
-      res.redirect('/' + getVersion(req.query.version) + '/service/service-page-logged-in')
+      res.redirect('/' + getVersion(req) + '/service/service-page-logged-in')
     }
     else {
       // Send user to ineligible page
-      res.redirect('/' + getVersion(req.query.version) + '/service/service-page-consent')
+      res.redirect('/' + getVersion(req) + '/service/service-page-consent')
     }
 })
 
@@ -166,15 +170,15 @@ router.post('/*/login/login-triage', function (req, res) {
     // Check whether the variable matches a condition
     if (triage == "AsYourself"){
       // Send user to next page
-      res.redirect('/' + getVersion(req.query.version) + '/service/service-logged-in')
+      res.redirect('/' + getVersion(req) + '/service/service-logged-in')
     }
     if (triage == "AsSomeoneElse"){
         // Send user to next page
-        res.redirect('/' + getVersion(req.query.version) + '/login/login-as-list')
+        res.redirect('/' + getVersion(req) + '/login/login-as-list')
     }
       if (triage == "Delegate"){
         // Send user to next page
-        res.redirect('/' + getVersion(req.query.version) + '/add-delegate/delegate-preflight')
+        res.redirect('/' + getVersion(req) + '/add-delegate/delegate-preflight')
     }
 })
 
@@ -186,15 +190,15 @@ router.post('/*/login/login-as-list', function (req, res) {
     // Check whether the variable matches a condition
     if (delegate == "Alice"){
       // Send user to next page
-      res.redirect('/' + getVersion(req.query.version) + '/service/service-logged-in')
+      res.redirect('/' + getVersion(req) + '/service/service-logged-in')
     }
     if (delegate == "Tom"){
         // Send user to next page
-        res.redirect('/' + getVersion(req.query.version) + '/service/service-logged-in')
+        res.redirect('/' + getVersion(req) + '/service/service-logged-in')
     }
       if (delegate == "Harriet"){
         // Send user to next page
-        res.redirect('/' + getVersion(req.query.version) + '/service/service-logged-in')
+        res.redirect('/' + getVersion(req) + '/service/service-logged-in')
     }
 })
 
