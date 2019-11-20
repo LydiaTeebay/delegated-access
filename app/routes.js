15f9ef47671605c20323f7eda944da1057ad76c0
Lydia Teebay <lydia@hippodigital.co.uk>
2019-11-20 15:00:27 +0000

    consent success email

--- app/routes.js
+++ app/routes.js
@@ -174,7 +174,7 @@ router.post('/v4/primary-user-details-and-relationship', function (req, res) {
       // this long string is the template ID, copy it from the template
       // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
       // in your code.
-      '5d6649e3-b6df-4fc5-96e0-af80e3bec737',
+      'aa5440eb-1d3c-4d31-8b32-973df5119c40',
       // `emailAddress` here needs to match the name of the form field in
       // your HTML page
       req.body.emailAddress, { 
@@ -191,7 +191,7 @@ router.post('/v4/primary-user-details-and-relationship', function (req, res) {
   
     // This is the URL the users will be redirected to once the email
     // has been sent
-    res.redirect('/v4/consent-success');
+    res.redirect('/v4/service/consent-success');
   
   })
 
