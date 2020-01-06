266b1ab484bc1b325e284d0e84e118084014344d
Lydia Teebay <lydia@hippodigital.co.uk>
2020-01-06 16:29:39 +0000

    prototype versions

--- app/routes.js
+++ app/routes.js
@@ -132,17 +132,17 @@ router.post('/*/add-delegate/delegate-relationship', function(req, res) {
 // V4 Emails  
 // Take user details and send the invite email
 
-router.get('../v4/primary-user-details-and-relationship', function (req, res) {
+router.get('../*/primary-user-details-and-relationship', function (req, res) {
     var emailSent = req.query.emailSent
     console.log('render', req.query.emailSent)
-    res.render('../v4/primary-user-details-and-relationship', {emailSent: emailSent}, function(err,html) {
+    res.render('../*/primary-user-details-and-relationship', {emailSent: emailSent}, function(err,html) {
         res.send(html)
     })
 })
 
 // The URL here needs to match the URL of the page that the user is on
 // when they type in their email address 
-router.post('../v4/primary-user-details-and-relationship', function (req, res) {
+router.post('../*/primary-user-details-and-relationship', function (req, res) {
 
     notify.sendEmail(
       // this long string is the template ID, copy it from the template
@@ -165,23 +165,23 @@ router.post('../v4/primary-user-details-and-relationship', function (req, res) {
   
     // This is the URL the users will be redirected to once the email
     // has been sent
-    res.redirect('../v4/primary-user-details-and-relationship?emailSent=true');
+    res.redirect('../*/primary-user-details-and-relationship?emailSent=true');
   
   })
 
   // Send an email after accepting the invite
 
-router.get('../v4/service/consent', function (req, res) {
+router.get('../*/service/consent', function (req, res) {
     var emailSent = req.query.emailSent
     console.log('render', req.query.emailSent)
-    res.render('../v4/service/consent', {emailSent: emailSent}, function(err,html) {
+    res.render('../*/service/consent', {emailSent: emailSent}, function(err,html) {
         res.send(html)
     })
 })
 
 // The URL here needs to match the URL of the page that the user is on
 // when they type in their email address
-router.post('../v4/service/consent', function (req, res) {
+router.post('../*/service/consent', function (req, res) {
     console.log (req.body)
 
     notify.sendEmail(
@@ -206,94 +206,14 @@ router.post('../v4/service/consent', function (req, res) {
   
     // This is the URL the users will be redirected to once the email
     // has been sent
-    res.redirect('../v4/service/consent-success?emailSent=true');
+    res.redirect('../*/service/consent-success?emailSent=true');
   
   })
 
 
-  // V5 Emails
-  // Take user details and send the invite email
-
-router.get('../v5/primary-user-details-and-relationship', function (req, res) {
-    var emailSent = req.query.emailSent
-    console.log('render', req.query.emailSent)
-    res.render('../v5/primary-user-details-and-relationship', {emailSent: emailSent}, function(err,html) {
-        res.send(html)
-    })
-})
-
-// The URL here needs to match the URL of the page that the user is on
-// when they type in their email address 
-router.post('../v5/primary-user-details-and-relationship', function (req, res) {
-
-    notify.sendEmail(
-      // this long string is the template ID, copy it from the template
-      // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
-      // in your code.
-      '5d6649e3-b6df-4fc5-96e0-af80e3bec737',
-      // `emailAddress` here needs to match the name of the form field in
-      // your HTML page
-      req.body.emailAddress, { 
-          personalisation: {
-          'primaryuserfirstname': req.body.primaryUserFirstName,
-          'primaryuserlastname': req.body.primaryUserLastName 
-        },
-        reference: ''
-      })
-      .then(response => console.log('response'))
-      .catch(err => console.error('error', err))
-
-    console.log(req.body.emailAddress)
-  
-    // This is the URL the users will be redirected to once the email
-    // has been sent
-    res.redirect('../v5/primary-user-details-and-relationship?emailSent=true');
-  
-  })
-
-  // Send an email after accepting the invite
-
-router.get('../v5/service/consent', function (req, res) {
-    var emailSent = req.query.emailSent
-    console.log('render', req.query.emailSent)
-    res.render('../v5/service/consent', {emailSent: emailSent}, function(err,html) {
-        res.send(html)
-    })
-})
-
-// The URL here needs to match the URL of the page that the user is on
-// when they type in their email address
-router.post('../v5/service/consent', function (req, res) {
-    console.log (req.body)
-
-    notify.sendEmail(
-      // this long string is the template ID, copy it from the template
-      // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
-      // in your code.
-      'aa5440eb-1d3c-4d31-8b32-973df5119c40',
-      // `emailAddress` here needs to match the name of the form field in
-      // your HTML page
-      req.body.emailAddress, { 
-          personalisation: {
-          'primaryuserfirstname': req.body.primaryUserFirstName,
-          'primaryuserlastname': req.body.primaryUserLastName,
-          'relationship': req.body.relationship
-        },
-        reference: ''
-      })
-      .then(response => console.log('response'))
-      .catch(err => console.error('err'))
-
-    console.log(req.body.emailAddress)
-  
-    // This is the URL the users will be redirected to once the email
-    // has been sent
-    res.redirect('../v5/service/consent-success?emailSent=true');
-  
-  })
-
 
   
+
 
   // Select who to log in as
 
