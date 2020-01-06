b81699ec3de0afd469143c54436f4781eb7d20f9
Lydia Teebay <lydia@hippodigital.co.uk>
2020-01-06 16:41:42 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -130,19 +130,20 @@ router.post('/*/add-delegate/delegate-relationship', function(req, res) {
 
 
 // V4 Emails  
+
 // Take user details and send the invite email
 
-router.get('../v5/primary-user-details-and-relationship', function (req, res) {
+router.get('/*/primary-user-details-and-relationship', function (req, res) {
     var emailSent = req.query.emailSent
     console.log('render', req.query.emailSent)
-    res.render('../v5/primary-user-details-and-relationship', {emailSent: emailSent}, function(err,html) {
+    res.render('/*/primary-user-details-and-relationship', {emailSent: emailSent}, function(err,html) {
         res.send(html)
     })
 })
 
 // The URL here needs to match the URL of the page that the user is on
 // when they type in their email address 
-router.post('../v5/primary-user-details-and-relationship', function (req, res) {
+router.post('/*/primary-user-details-and-relationship', function (req, res) {
 
     notify.sendEmail(
       // this long string is the template ID, copy it from the template
@@ -165,23 +166,23 @@ router.post('../v5/primary-user-details-and-relationship', function (req, res) {
   
     // This is the URL the users will be redirected to once the email
     // has been sent
-    res.redirect('../v5/primary-user-details-and-relationship?emailSent=true');
+    res.redirect('/*/primary-user-details-and-relationship?emailSent=true');
   
   })
 
   // Send an email after accepting the invite
 
-router.get('../v5/service/consent', function (req, res) {
+router.get('/*/service/consent', function (req, res) {
     var emailSent = req.query.emailSent
     console.log('render', req.query.emailSent)
-    res.render('../v5/service/consent', {emailSent: emailSent}, function(err,html) {
+    res.render('/*/service/consent', {emailSent: emailSent}, function(err,html) {
         res.send(html)
     })
 })
 
 // The URL here needs to match the URL of the page that the user is on
 // when they type in their email address
-router.post('../v5/service/consent', function (req, res) {
+router.post('/*/service/consent', function (req, res) {
     console.log (req.body)
 
     notify.sendEmail(
@@ -206,7 +207,7 @@ router.post('../v5/service/consent', function (req, res) {
   
     // This is the URL the users will be redirected to once the email
     // has been sent
-    res.redirect('../v5/service/consent-success?emailSent=true');
+    res.redirect('/*/service/consent-success?emailSent=true');
   
   })
 
